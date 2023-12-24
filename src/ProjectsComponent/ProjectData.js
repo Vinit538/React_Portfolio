900e4a9d88385e845861280feddc528a2aecb2d1
Vinit538 <vinitkaratagi538@gmail.com>
2023-12-25 02:44:40 +0530

    fiveth commit

---
+++ src/ProjectsComponent/ProjectData.js
@@ -0,0 +1,36 @@
+const ProjectData=[
+{
+    id:1,
+    projectImage1:'LearnGuruProject.jpg',
+    projectType1:'Full Stack Project',
+    projectTitle1:'Learn Guru',
+    projectDescription1:'"In Learn Guru project, served as a dedicated learner and Trainer, fostering a collaborative environment for seamless knowledge flow. Through curated learning experiences, empowered individuals to grow and excel in their pursuits."',
+    projectSkills1:'React JS, Java, Spring Boot, My Sql',
+    projectGitLink1:'',
+    projectWebLink1:'',
+    projectFolderLink1:''
+},
+{
+    id:2,
+    projectImage2:'IconographyProject.jpg',
+    projectType2:'Front-End Project',
+    projectTitle2:'Iconography',
+    projectDescription2:'"Iconography it is web application clone and Showcase is a curated collection of stunning images, each telling its own story with locations, captivating descriptions."',
+    projectSkills2:'React JS, Java Script,CSS, ES6',
+    projectGitLink2:'',
+    projectWebLink2:'',
+    projectFolderLink2:''
+},
+{
+    id:3,
+    projectImage3:'DSProject.jpg',
+    projectType3:'Implementing linear data structures',
+    projectTitle3:'A STAND ALONE APPLICATION',
+    projectDescription3:'" The Main agenda of the project is to showcase the operations and working of different linear data structures ."',
+    projectSkills3:'Java, OOPS,Swing, Eclipse IDE,Window Builder.',
+    projectGitLink3:'',
+    projectWebLink3:'',
+    projectFolderLink3:''
+},  
+]
+export default ProjectData;
--- src/ProjectsComponent/Projects.js
+++ src/ProjectsComponent/Projects.js
@@ -5,8 +5,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFolder } from "@fortawesome/free-regular-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
-
+import ProjectData from "./ProjectData.js";
 const Projects = () => {
+  const pData = ProjectData;
+
   return (
     <div className="ProjectsPage project">
       <div>
@@ -15,12 +17,15 @@ const Projects = () => {
       <div className="Project">
         <div className="projectContainer">
           <div className="ProjectOne">
-            <img src="./Images/LearnGuruProject.png" className="projectImage" />
+            <img
+              src="./Images/LearnGuruProject.png"
+              className="projectImage"
+              alt="LearnGuruProject"
+            />
           </div>
           <div className="projectInfo">
             <span className="projectType">Full Stack Project</span>
             <span className="projectTitle">Learn Guru</span>
-            {/* <span  className='projectSubTitle'>SubTitle</span> */}
             <span className="projectCard">
               "In Learn Guru project, I served as a dedicated learner and
               Trainer, fostering a collaborative environment for seamless
@@ -28,63 +33,185 @@ const Projects = () => {
               individuals to grow and excel in their pursuits."
             </span>
             <span className="projectSkills">
-            <span>React JS,</span>
-            <span>Java,</span>
-            <span>Spring Boot,</span>
-            <span>My Sql</span>
-             </span>
+              <span>React JS,</span>
+              <span>Java,</span>
+              <span>Spring Boot,</span>
+              <span>My Sql</span>
+            </span>
             <span>
               <span className="projectLinks">
                 <span>
-                  <FontAwesomeIcon
-                    className="projectIcon"
-                    icon={faFolder}
-                    size="2xl"
-                    // style={{ color: "#5eafc9" }}
-                  />
-                </span>
-                  <span>
+                  <a
+                    href="https://github.com/Vinit538/Learn-Guru"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     <FontAwesomeIcon
+                      icon={faFolder}
+                      size="2xl"
                       className="projectIcon"
+                    />
+                  </a>
+                </span>
+                <span>
+                  <a
+                    href="https://learn-guru.vercel.app/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FontAwesomeIcon
                       icon={faUpRightFromSquare}
                       size="2xl"
-                    //   style={{ color: "#1f51a8" }}
+                      className="projectIcon"
                     />
-                  </span>
-                  <span>
+                  </a>
+                </span>
+                <span>
+                  <a
+                    href="https://github.com/Vinit538/Learn-Guru"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     <FontAwesomeIcon
-                    className="projectIcon"
                       icon={faGithub}
                       size="2xl"
-                    //   style={{ color: "#2755a5" }}
+                      className="projectIcon"
                     />
-                  </span>
+                  </a>
+                </span>
               </span>
             </span>
           </div>
         </div>
         <div className="projectContainer">
           <div className="projectInfo2">
-            <span className="projectTitle">Project Title</span>
-            <span className="projectSubTitle">SubTitle</span>
-            <span className="projectCard2">Project Card</span>
-            <span className="projectSkills">Skills</span>
+            <span className="projectType">Front-End Project</span>
+            <span className="projectTitle">Iconography</span>
+            <span className="projectCard2">
+              "Iconography it is web application clone and Showcase is a curated
+              collection of stunning images, each telling its own story with
+              locations, captivating descriptions."
+            </span>
+            <span className="projectSkills">
+              <span>React JS,</span>
+              <span>Java Script,</span>
+              <span>ES6,</span>
+              <span>CSS</span>
+            </span>
+            <span>
+              <span className="projectLinks">
+                <span>
+                  <a
+                    href="https://github.com/Vinit538/Iconography"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FontAwesomeIcon
+                      icon={faFolder}
+                      size="2xl"
+                      className="projectIcon"
+                    />
+                  </a>
+                </span>
+                <span>
+                  <a
+                    href="https://iconography.vercel.app/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FontAwesomeIcon
+                      icon={faUpRightFromSquare}
+                      size="2xl"
+                      className="projectIcon"
+                    />
+                  </a>
+                </span>
+                <span>
+                  <a
+                    href="https://github.com/Vinit538/Iconography"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FontAwesomeIcon
+                      icon={faGithub}
+                      size="2xl"
+                      className="projectIcon"
+                    />
+                  </a>
+                </span>
+              </span>
+            </span>
           </div>
 
           <div className="ProjectOne">
-            <img src="./Images/LearnGuruProject.png" className="projectImage2" />
+            <img
+              src="./Images/IconographyProject.png"
+              className="projectImage2"
+            />
           </div>
         </div>
         <div className="projectContainer">
           <div className="ProjectOne">
-            <img src="./Images/LearnGuruProject.png" className="projectImage" />
+            <img src="./Images/DSProject.png" className="projectImage" />
           </div>
           <div className="projectInfo">
-            <span className="projectType">Full Stack Project</span>
-            <span className="projectTitle">Learn Guru</span>
-            {/* <span  className='projectSubTitle'>SubTitle</span> */}
-            <span className="projectCard">Project Card</span>
-            <span className="projectSkills">React JS, Java,Spring Boot </span>
+            <span className="projectType">
+              Linear data structures
+            </span>
+            <span className="projectTitle" style={{fontSize:'25px'}}>STAND ALONE APPLICATION</span>
+            <span className="projectCard">
+              " The Main agenda of the project is to showcase the operations and
+              working of different linear data structures ."
+            </span>
+            <span className="projectSkills">
+              <span>Java,</span>
+              <span>OOPS,</span>
+              <span>Swing,</span>
+              <span>DS</span>
+            </span>
+            <span>
+              <span className="projectLinks">
+                <span>
+                  <a
+                    href="https://github.com/Vinit538/Data-Structure-Stand-Alone-Application"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FontAwesomeIcon
+                      icon={faFolder}
+                      size="2xl"
+                      className="projectIcon"
+                    />
+                  </a>
+                </span>
+                {/* <span>
+                  <a
+                    href="https://twitter.com/Vinit_Karatagi"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FontAwesomeIcon
+                      icon={faUpRightFromSquare}
+                      size="2xl"
+                    className="projectIcon"
+                    />
+                  </a>
+                </span> */}
+                <span>
+                  <a
+                    href="https://github.com/Vinit538/Data-Structure-Stand-Alone-Application"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FontAwesomeIcon
+                      icon={faGithub}
+                      size="2xl"
+                      className="projectIcon"
+                    />
+                  </a>
+                </span>
+              </span>
+            </span>
           </div>
         </div>
       </div>
