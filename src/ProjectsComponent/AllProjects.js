f98798e1038aee1bef06dc816a8146313059edd0
Vinit538 <vinitkaratagi538@gmail.com>
2024-01-04 00:04:28 +0530

    update 2

--- src/ProjectsComponent/AllProjects.js
+++ src/ProjectsComponent/AllProjects.js
@@ -5,6 +5,7 @@ const AllProjects=[
     projectTitle:'Student Management System Application',
     projectDescription:' This is Back End Project it is Developed in Spring boot application using HTML,CSS and MySQL for performing CRUD operations in a Student Management System.',
     projectSkills:'Java, Spring Boot, My Sql,HTML,CSS',
+    projectContent: 'The Student Management System Application is designed to efficiently manage student-related information. It allows for the creation, retrieval, update, and deletion (CRUD) operations on student records. The application uses Spring Boot for the backend, providing a robust and scalable system for educational institutions.',
     projectGitLink:'https://github.com/Vinit538/StudentManagementSystem',
     projectWebLink:'',
     projectFolderLink:''
@@ -14,6 +15,7 @@ const AllProjects=[
     projectTitle:'Lead Tracker',
     projectDescription:'It is Chrome Extension is to save the current tab url and also user input links in browser.',
     projectSkills:'Html, CSS, Java Script',
+    projectContent: 'Lead Tracker is a Chrome Extension that simplifies the process of saving and organizing links. It allows users to save the current tab URL with just one click and input custom links. This extension is useful for individuals who want to keep track of important web pages or leads while browsing.',
     projectGitLink:'https://github.com/Vinit538/Leads-Tracker',
     projectWebLink:'',
     projectFolderLink:''
@@ -23,6 +25,7 @@ const AllProjects=[
     projectTitle:'VPN Shopping',
     projectDescription:' This project, web based application intended for online retailers. The main objective of this application is to make it interactive and its easy of use',
     projectSkills:'Html, CSS, Java Script,SQL',
+    projectContent:"The VPN Shopping is a web based application intended for online retailers. The main objective of this application is to make it interactive and its ease of use. It would make searching, viewing and selection of a product easier. It contains a sophisticated Category’s for user's to search for products specific to their needs. The user can view the complete specification of each product. The user want to buy the products then user first Register VPN Shopping and then user login to buy the which Product needed  and user can write their own reviews ",
     projectGitLink:'https://github.com/Vinit538/VPN-Shopping-Project',
     projectWebLink:'',
     projectFolderLink:''
@@ -32,6 +35,7 @@ const AllProjects=[
     projectTitle:'Smart Health Monitoring System',
     projectDescription:'It is IOT based project Which contain energy Hervisting System, run on solor power and also battery where doctor can also monitor the patient helath condition from online in remotely.',
     projectSkills:'IOT, C++, Ardunio, GSM Module',
+    projectContent:"A Smart health monitoring system using IOT is proposed where the authorized Doctor or person can access these data stored using IOT platform and based on these values received, the diseases are diagnosed by the doctors from a far distance. The core objective of this project is the design and implementation of a smart patient health tracking system that uses Sensors to track patient health and uses internet to inform their loved ones in case of any issues. The objective of developing monitoring systems is to reduce health care costs by reducing physician office visits, hospitalizations, and diagnostic testing procedure",
     projectGitLink:'https://github.com/Vinit538/Smart-Health-Monitoring-System-Project',
     projectWebLink:'',
     projectFolderLink:''
--- src/ProjectsComponent/ProjectCards.js
+++ src/ProjectsComponent/ProjectCards.js
@@ -1,63 +1,97 @@
-import React ,{useState} from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFolder} from '@fortawesome/free-regular-svg-icons';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faUpRightFromSquare} from '@fortawesome/free-solid-svg-icons';
-import AllProjects from './AllProjects.js'
+
+
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFolder } from "@fortawesome/free-regular-svg-icons";
+import { faGithub, faExternalLinkAlt } from "@fortawesome/free-brands-svg-icons";
+import { faUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 import { Tooltip as ReactTooltip } from "react-tooltip";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import AllProjects from "./AllProjects.js";
 
 const ProjectCards = () => {
+  const [pData, setPData] = useState(AllProjects);
+  const [modal, setModal] = useState(false);
+  const [selectedProject, setSelectedProject] = useState(null);
 
-    const [pData, setPData] = useState(AllProjects);
-    console.log(pData);
+  const toggle = (project) => {
+    setSelectedProject(project);
+    setModal(!modal);
+  };
 
-    return (
-    <div className='projectCardGrid'>
-       {pData.map((project)=> (
-                <div className='projectsCard' key={project.id}>
-                <div className='projectCardLinks'>
-                   <span className='projectModal projectIcon'><FontAwesomeIcon icon={faFolder} size="2xl"  />{}</span>
-                   <span className='projectLink projectIcon'><FontAwesomeIcon icon={faUpRightFromSquare} size="2xl"/></span>
-                  {project.projectGitLink &&(
-                     <span className='projectGit projectIcon' >  <a
-                     href={project.projectGitLink} target="_blank"rel="noopener noreferrer"
-                   >
-                     <FontAwesomeIcon
-                       icon={faGithub}
-                       size="2xl"
-                       className="projectIcon"
-                       data-tooltip-id="my-tooltip-1"
-                     />
-                   </a></span> 
-                  )}
-                </div>
-                <div className='projectContent'>
-                 <span className='projectCardTitle'>
-                    {project.projectTitle}
-                 </span><br/>
-                 <span className='projectCardDescription'>
-                    {project.projectDescription}
-                 </span>
-                </div>
-                 <div className='projectSkills'>
-                         {project.projectSkills}
-                 </div>
-             </div>
+  return (
+    <div className="projectCardGrid">
+      {pData.map((project) => (
+        <div className="projectsCard" key={project.id}>
+          <div className="projectCardLinks">
+           
+              <span className="projectModal projectIcon">
+              <FontAwesomeIcon
+                icon={faFolder}
+                size="2xl"
+                onClick={() => toggle(project)}
+                data-tooltip-id="my-tooltip-2"
+              />
+              {}
+            </span>
+           
+          {
+            project.projectWebLink &&(
+              <span className="projectLink projectIcon">
+              <FontAwesomeIcon icon={faUpRightFromSquare} size="2xl" />
+            </span>
+            )
+          }
+            {project.projectGitLink && (
+              <span className="projectGit projectIcon">
+                {" "}
+                <a
+                  href={project.projectGitLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon
+                    icon={faGithub}
+                    size="2xl"
+                    className="projectIcon"
+                    data-tooltip-id="my-tooltip-1"
+                  />
+                </a>
+              </span>
+            )}
+          </div>
+          <div className="projectContent">
+            <span className="projectCardTitle">{project.projectTitle}</span>
+            <br />
+            <span className="projectCardDescription">
+              {project.projectDescription}
+            </span>
+          </div>
+          <div className="projectSkills">{project.projectSkills}</div>
+        </div>
+      ))}
+      {selectedProject && (
+        <Modal isOpen={modal} toggle={() => toggle(null)} centered={true} className="modalClass">
+          <ModalHeader toggle={() => toggle(null)} className="projectCardTitle">
+            {selectedProject.projectTitle}
+          </ModalHeader>
+          <ModalBody className="modalBody"> 
+              <p className="projectCardDescription"> {selectedProject.projectDescription}</p>
+              <p className="projectCardDescription">{selectedProject.projectContent}</p>
+              <p className="projectSkills">{selectedProject.projectSkills}</p>
+            </ModalBody>
 
-            ))}
-            <ReactTooltip
-        id="my-tooltip-1"
-        place="bottom"
-        content="Git Link"
-        variant="info"
-      />
-      <ReactTooltip
-        id="my-tooltip-2"
-        place="bottom"
-        variant="info"
-        content="Git Link"
-      />
+          <ModalFooter>
+            <Button color="secondary" onClick={() => toggle(null)}>
+              Cancel
+            </Button>
+          </ModalFooter>
+        </Modal>
+      )}
+      <ReactTooltip id="my-tooltip-1" place="bottom" content="Git Link" variant="info" />
+      <ReactTooltip id="my-tooltip-2" place="bottom" variant="info" content="Modal View" />
     </div>
-  )
-}
+  );
+};
+
 export default ProjectCards;
